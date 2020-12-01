763d88ecc169e455e7bdc3b5bb2d12f86199467c
thanhphu <tphu2000@gmail.com>
2020-12-01 21:17:47 +0700

    push code

---
+++ quiz-management/SQL/QuizDB.sql
@@ -0,0 +1,162 @@
+﻿CREATE TABLE nguoiDung
+(
+	maNguoiDung INT IDENTITY,
+	tenTaiKhoan VARCHAR(20) UNIQUE,
+	matKhau VARCHAR(30),
+	phanQuyen INT,
+	TrangThai INT
+	
+	CONSTRAINT PK_nguoiDung
+	PRIMARY KEY (maNguoiDung)
+)
+
+
+CREATE TABLE thongTin
+(
+	maNguoidung INT,
+	tenNguoiDung NVARCHAR(50),
+	ngaySinh DATE,
+	
+	CONSTRAINT PK_thongtin
+	PRIMARY KEY (maNguoidung)
+)
+
+ALTER TABLE dbo.thongTin ADD CONSTRAINT FK_thongtin_nguoidung FOREIGN KEY(maNguoidung) REFERENCES dbo.nguoiDung(maNguoidung)
+
+CREATE TABLE monHoc
+(
+	maMonHoc INT IDENTITY,
+	tenMonHoc NVARCHAR(20)
+
+	CONSTRAINT PK_monHoc
+	PRIMARY KEY(maMonHoc)
+)
+
+CREATE TABLE boDe
+(
+	maBoDe INT IDENTITY,
+	thoiGian DATE,
+	tongSoCau INT,
+	maMon INT
+
+	CONSTRAINT PK_boDe
+	PRIMARY KEY (maBoDe)
+)
+
+ALTER TABLE dbo.boDe ADD CONSTRAINT FK_boDe_monHoc FOREIGN KEY(maMon) REFERENCES dbo.monHoc(maMonHoc)
+
+CREATE TABLE cauHoi
+(
+	maCauHoi INT IDENTITY,
+	maMonHoc INT,
+	cauHoi NVARCHAR(100),
+	doKho INT
+
+	CONSTRAINT PK_cauHoi
+	PRIMARY KEY (maCauHoi)
+)
+
+CREATE TABLE cTBoDe
+(
+	maBoDe INT,
+	maCauHoi INT
+
+	CONSTRAINT PK_cTBoDe
+	PRIMARY KEY (maBoDe,maCauHoi)
+)
+
+ALTER TABLE dbo.cTBoDe ADD CONSTRAINT FK_cTBoDe_boDe FOREIGN KEY(maBoDe) REFERENCES dbo.boDe (maBoDe)
+ALTER TABLE  dbo.cTBoDe ADD CONSTRAINT FK_ctBoDe_cauHoi FOREIGN KEY (maCauHoi) REFERENCES dbo.cauHoi(maCauHoi)
+
+CREATE TABLE dapAn
+(
+	maCauHoi INT,
+	maCauTraloi INT,
+	cauTraLoi NVARCHAR(100),
+	dapAn INT,
+	 
+	CONSTRAINT PK_dapAn
+	PRIMARY KEY (maCauHoi,maCauTraLoi)
+)
+
+
+ALTER TABLE dbo.dapAn ADD CONSTRAINT FK_dapan_cauHoi FOREIGN KEY (maCauHoi) REFERENCES dbo.cauHoi (maCauHoi)
+
+CREATE TABLE ketQua
+(
+	maKetQua INT IDENTITY,
+	maNguoiDung INT,
+	maBoDe INT,
+	cauDung INT,
+	cauSai INT,
+	chuaLam INT,
+	ngayLam DATE,
+	trangThai INT,
+	thoiGian INT,
+	diem FLOAT
+    
+	CONSTRAINT PK_ketQua
+	PRIMARY KEY (maKetQua)
+)
+
+ALTER TABLE dbo.ketQua ADD CONSTRAINT FK_ketQua_nguoiDung FOREIGN KEY(maNguoiDung) REFERENCES dbo.nguoiDung(maNguoiDung)
+ALTER TABLE  dbo.ketQua ADD CONSTRAINT FK_ketQua_boDe FOREIGN KEY(maBoDe) REFERENCES dbo.boDe(maBoDe)
+
+CREATE TABLE cTKetQua
+(
+    maKetQua INT,
+	maCauHoi INT,
+	maCauTraLoi INT,
+	thoiGian INT
+
+	CONSTRAINT PK_cTKetQua
+	PRIMARY KEY (maKetQua,maCauHoi,maCauTraLoi)
+)
+
+ALTER TABLE dbo.cTKetQua ADD CONSTRAINT FK_cTKetQua_ketQua FOREIGN KEY (maKetQua) REFERENCES dbo.ketQua(maKetQua)
+ALTER TABLE  dbo.cTKetQua ADD CONSTRAINT FK_cTKetQua_cauHoi FOREIGN KEY (maCauHoi) REFERENCES dbo.cauHoi(maCauHoi)
+ALTER TABLE  dbo.cTKetQua ADD CONSTRAINT FK_cTKetQua_dapAn FOREIGN KEY (maCauHoi,maCauTraLoi) REFERENCES dbo.dapAn(maCauHoi,maCauTraloi)
+
+CREATE TABLE lichThi
+(
+	maNguoiDung INT,
+	maMonHoc INT,
+	ngayThi DATE
+
+	CONSTRAINT PK_lichThi
+	PRIMARY KEY(maNguoiDung,maMonHoc,ngayThi)
+)
+
+CREATE TABLE luyenTap
+(
+	maNguoiDung INT,
+	soCauDung INT,
+	soCauSai INT,
+	ngay date
+)
+ALTER TABLE  dbo.luyenTap ADD CONSTRAINT FK_luyenTap_nguoiDung FOREIGN KEY(maNguoiDung) REFERENCES dbo.nguoiDung(maNguoiDung)
+
+CREATE TABLE dongGop
+(
+	maDongGop INT IDENTITY,
+	maNguoiDung INT,
+	maMonHoc INT,
+	trangthai INT,
+	ngay DATE,
+	cauHoi NVARCHAR(100)
+
+	CONSTRAINT PK_dongGop
+	PRIMARY KEY (maDongGop)
+)
+
+ALTER TABLE dbo.dongGop ADD CONSTRAINT FK_dongGop_nguoiDung FOREIGN KEY(maNguoiDung) REFERENCES dbo.nguoiDung(maNguoiDung)
+ALTER TABLE  dbo.dongGop ADD CONSTRAINT FK_dongGop_monHoc FOREIGN KEY(maMonHoc) REFERENCES dbo.monHoc (maMonHoc)
+
+CREATE TABLE cTDongGop
+(
+    maDongGop INT,
+	cauTraLoi NVARCHAR(100),
+	dapAn INT
+)
+
+ALTER TABLE dbo.cTDongGop ADD CONSTRAINT FK_cTDongGop_dongGop FOREIGN KEY(maDongGop) REFERENCES dbo.dongGop(maDongGop)
