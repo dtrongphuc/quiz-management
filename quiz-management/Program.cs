c2034c7f0cfd7081407d09ab407df7ef83562716
nguyenhieunghia2001 <67048599+nguyenhieunghia2001@users.noreply.github.com>
2020-12-09 08:04:49 +0700

    chỉnh  sửa trang đề thi thử

--- quiz-management/Program.cs
+++ quiz-management/Program.cs
@@ -18,7 +18,7 @@ namespace quiz_management
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new LoginView());
+            Application.Run(new PracticExamView());
         }
     }
 }
