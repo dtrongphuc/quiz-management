98bd253d50e643e5ae44c3a45e138e67ab6e8dae
ThanhPhu-Dev <tphu2000@gmail.com>
2020-12-15 11:09:49 +0700

    liên Kết MainView với ResultView và ScheludeView

--- quiz-management/Program.cs
+++ quiz-management/Program.cs
@@ -23,7 +23,7 @@ namespace quiz_management
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new TestScheduleView(1));
+            Application.Run(new LoginView());
         }
     }
 }
