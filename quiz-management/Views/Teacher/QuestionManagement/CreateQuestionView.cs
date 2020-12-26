0fed579a2947c3a08f89ee72bbf89040ad7f7d95
nguyenhieunghia2001 <67048599+nguyenhieunghia2001@users.noreply.github.com>
2020-12-26 13:21:54 +0700

    tạo đề thi

---
+++ quiz-management/Views/Teacher/QuestionManagement/CreateQuestionView.cs
@@ -0,0 +1,100 @@
+﻿using quiz_management.Models;
+using quiz_management.Presenters.Teacher.QuestionManagement;
+using quiz_management.Views.Teacher.Main;
+using quiz_management.Views.Teacher.PaperManagement;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace quiz_management.Views.Teacher.QuestionManagement
+{
+    public partial class CreateQuestionView : Form, ICreateQuestionView
+    {
+        CreateQuestionPresneter presneter;
+        public CreateQuestionView(int code)
+        {
+            InitializeComponent();
+            presneter = new CreateQuestionPresneter(this, code);
+            btnSend.Click += (_, e) =>
+            {
+                Create?.Invoke(btnSend, e);
+            };
+            btnShowQuestionList.Click += (_, e) =>
+            {
+                ShowListQuestion?.Invoke(btnShowQuestionList, e);
+            };
+            linkGobackMain.Click += (_, e) =>
+            {
+                GoBackMain?.Invoke(linkGobackMain, e);
+            };
+        }
+
+        public string TeacherName { set => lbTeacher.Text = value; }
+
+        public string Question => tbQuestion.Text;
+
+        public string GradeId => cbbLevel.SelectedValue.ToString();
+
+        public string SubjectId => cbbSubject.SelectedValue.ToString();
+
+        public string AnswerA => tbAnswerA.Text;
+
+        public string AnswerB => tbAnswerB.Text;
+
+        public string AnswerC => tbAnswerC.Text;
+
+        public string AnswerD => tbAnswerD.Text;
+
+        public string AnswerE => tbAnswerE.Text;
+
+        public string AnswerF => tbAnswerF.Text;
+
+        public List<khoiLop> GradeList { set => cbbLevel.DataSource = value; }
+        public List<monHoc> SubjectList { set => cbbSubject.DataSource = value; }
+
+        public string lvDifficute => cbbDifficute.Text;
+
+        bool ICreateQuestionView.cbResultA => cbResultA.Checked;
+
+        bool ICreateQuestionView.cbResultB => cbResultB.Checked;
+                                              
+        bool ICreateQuestionView.cbResultC => cbResultC.Checked;
+                                              
+        bool ICreateQuestionView.cbResultD => cbResultD.Checked;
+                                            
+        bool ICreateQuestionView.cbResultE => cbResultE.Checked;
+                                            
+        bool ICreateQuestionView.cbResultF => cbResultF.Checked;
+
+        public event EventHandler Create;
+        public event EventHandler GoBackMain;
+        public event EventHandler ShowListQuestion;
+
+        public void ShowMainTeacher(int code)
+        {
+            this.Hide();
+            MainTeacherView screen = new MainTeacherView(code);
+            screen.FormClosed += (_, e) => this.Close();
+            screen.Show();
+        }
+
+        public void ShowMessage(string text)
+        {
+            MessageBox.Show(text, "Thông báo");
+        }
+
+        public void ShowQuestionList(int code)
+        {
+            //this.Hide();
+            //PaperListView screen = new PaperListView(code);
+            //screen.FormClosed += (_, e) => this.Close();
+            //screen.Show();
+        }
+    }
+}
---
+++ quiz-management/Views/Teacher/QuestionManagement/ICreateQuestionView.cs
@@ -0,0 +1,42 @@
+﻿using quiz_management.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace quiz_management.Views.Teacher.QuestionManagement
+{
+    interface ICreateQuestionView
+    {
+        string TeacherName { set; }
+        string Question { get; }
+        string GradeId { get; }
+        string SubjectId { get; }
+        string lvDifficute { get; }
+        string AnswerA { get; }
+        string AnswerB { get; }
+        string AnswerC { get; }
+        string AnswerD { get; }
+        string AnswerE { get; }
+        string AnswerF { get; }
+
+        bool cbResultA { get; }
+        bool cbResultB { get; }
+        bool cbResultC { get; }
+        bool cbResultD { get; }
+        bool cbResultE { get; }
+        bool cbResultF { get; }
+
+        List<khoiLop> GradeList { set; }
+        List<monHoc> SubjectList { set; }
+
+        event EventHandler Create;
+        event EventHandler GoBackMain;
+        event EventHandler ShowListQuestion;
+
+        void ShowMainTeacher(int code);
+        void ShowQuestionList(int code);
+        void ShowMessage(string text);
+    }
+}
