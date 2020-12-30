ac8ca4463942f9a41a2adc4725971b0087da609f
nguyenhieunghia2001 <67048599+nguyenhieunghia2001@users.noreply.github.com>
2020-12-30 15:43:44 +0700

    binding danh sách kì thi thử

---
+++ quiz-management/Models/MockExam.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace quiz_management.Models
+{
+    public class MockExam
+    {
+        int _sTT;
+        string _subject;
+        string _grade;
+        string _startDay;
+        string _endDay;
+        string _totalStudent;
+
+        public int STT { get => _sTT; set => _sTT = value; }
+        public string Subject { get => _subject; set => _subject = value; }
+        public string Grade { get => _grade; set => _grade = value; }
+        public string StartDay { get => _startDay; set => _startDay = value; }
+        public string EndDay { get => _endDay; set => _endDay = value; }
+        public string TotalStudent { get => _totalStudent; set => _totalStudent = value; }
+    }
+}
